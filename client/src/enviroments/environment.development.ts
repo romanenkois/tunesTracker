9a711a181bbd5990f3dd05fed762cb8a65a6f49c
Illya <romanenkoisnot@gmail.com>
2024-10-24 21:14:03 +0300

    fix: privacy
    keys dont work no more
    dont even try

--- client/src/enviroments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  BASE_URL: 'https://api.spotify.com',
-  CLIENT_ID: '6f95aa1688124e4abd0e5c45862a7b10',
-  CLIENT_SECRET: 'dd25b70abaaf4e25a2d06e01b29751f9',
-  USER_ID: '31l6hgkzygyre6ijmn4xaqmbda3m'
-};
