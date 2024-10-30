7d6948a8cb41b71635062c27069c4ef3b3d938de
Illya <romanenkoisnot@gmail.com>
2024-10-30 18:16:20 +0200

    c/feat: user data rep

---
+++ client/src/app/data/repositories/user-data.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+
+
+@Injectable({providedIn: 'root'})
+export class UserDataRepository {
+  private readonly userCode: WritableSignal<string> = signal<string>('');
+  public setUserCode(code: string): void {
+    this.userCode.set(code);
+  }
+  public getUserCode(): string {
+    return this.userCode();
+  }
+
+
+}
