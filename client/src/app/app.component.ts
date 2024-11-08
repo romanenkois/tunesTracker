95853cfc9acdcee2078c78ed8a51b7b71be6343c
Illya <romanenkoisnot@gmail.com>
2024-11-08 19:19:23 +0200

    client/feat: top-navigation pannel

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { TopNavComponent } from "@widget/top-nav/top-nav.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, TopNavComponent],
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
 })
 export class AppComponent {
+  router: Router = inject(Router);
 
+  displayTopNav: boolean = true;
+
+  // used to stop showing the top nav pannel on login page
+  constructor() {
+    this.router.events.subscribe(() => {
+      this.displayTopNav = this.router.url !== '/login';
+    });
+  }
 }
