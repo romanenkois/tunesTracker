1e4b0c9346d4bd42a2f29d9d3870f9a5d22ba0c5
Illya <romanenkoisnot@gmail.com>
2024-11-13 16:33:05 +0200

    client/test: deploy

---
+++ client/src/enviroments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'http://localhost:3000/tunes-tracker-api',
+
+  SPOTIFY_CLIENT_ID: '6f95aa1688124e4abd0e5c45862a7b10',
+  SPOTIFY_CLIENT_SECRET: '',
+  SPOTIFY_REDIRECT_URI: 'http://localhost:4200/login',
+
+  ITEMS_LIMIT_PER_REQUEST: 25,
+};
