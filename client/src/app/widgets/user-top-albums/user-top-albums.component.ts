c29855d8bdb534bc83410d34fd1d227c32763898
Illya <romanenkoisnot@gmail.com>
2024-11-15 23:14:09 +0200

    client/feat: user top albums

--- client/src/app/widgets/user-top-albums/user-top-albums.component.ts
+++ client/src/app/widgets/user-top-albums/user-top-albums.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, WritableSignal, signal, computed } from '@angular/core';
+import { GetUserTopItemsCommand } from '@commands/get-user-top-items.command';
+import { TimeFrame } from '@entity/index';
+import { TimeframeSelectionComponent } from '@feature/timeframe-selection/timeframe-selection.component';
+import { UserTopItemsDataRepository } from '@repository/user-top-items.repository';
 
 @Component({
   selector: 'app-user-top-albums',
   standalone: true,
-  imports: [],
+  imports: [TimeframeSelectionComponent],
   templateUrl: './user-top-albums.component.html',
   styleUrl: './user-top-albums.component.scss'
 })
 export class UserTopAlbumsComponent {
+  private getUserTopItems: GetUserTopItemsCommand = inject(GetUserTopItemsCommand);
+  private userTopItemsRepository: UserTopItemsDataRepository = inject(UserTopItemsDataRepository);
 
+  userTopAlbums = computed(() => this.userTopItemsRepository.getUserTopAlbums());
+
+  periodOfTime: WritableSignal<TimeFrame> = signal('short_term');
+
+  changeTimeFrame(timeFrame: TimeFrame) {
+    if (this.periodOfTime() === timeFrame) {
+      return;
+    }
+    this.periodOfTime.set(timeFrame);
+
+    this.userTopItemsRepository.setUserTopAlbums([]);
+    this.getUserTopItems.getUserTopAlbums(this.periodOfTime());
+  }
+
+  constructor() {
+    this.getUserTopItems.getUserTopAlbums(this.periodOfTime());
+  }
 }
