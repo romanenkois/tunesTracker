83a34b1568f6a6a6b685e3503af0b0f55def8b2c
Illya <romanenkoisnot@gmail.com>
2024-11-15 19:53:36 +0200

    client/fix: config

--- client/src/app/shared/config/config.ts
+++ client/src/app/shared/config/config.ts
@@ -1,9 +1,10 @@
+const production = false;
+
 export const config = {
-  BASE_URL: 'http://localhost:3000/tunes-tracker-api',
+  BASE_URL: production ? 'https://tunes-tracker-backend.vercel.app/tunes-tracker-api' : 'http://localhost:3000/tunes-tracker-api',
+  SPOTIFY_REDIRECT_URI: production ? 'https://tunes-tracker-client.vercel.app/login' : 'http://localhost:4200/login',
 
   SPOTIFY_CLIENT_ID: '6f95aa1688124e4abd0e5c45862a7b10',
-  SPOTIFY_CLIENT_SECRET: '',
-  SPOTIFY_REDIRECT_URI: 'http://localhost:4200/login',
   SPOTIFY_STATE: '37',
 
   ITEMS_LIMIT_PER_REQUEST: 25,
