8a0913f921cda55e1eabfcef880a4587537b25c8
Illya <romanenkoisnot@gmail.com>
2024-11-27 19:38:02 +0200

    server/test

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 // env imports
 dotenv.config();
-const { config } = require('./shared/config/config');
+const { config } = require('./shared/config/config'); // should be done before dotenv config ????????
 
 const app = express();
 
@@ -25,28 +25,28 @@ v1Router.use('/albums', albumsRouter);
 v1Router.use('/artists', artistsRouter);
 v1Router.use('/tracks', tracksRouter);
 v1Router.use('/user-data', userDataRouter);
-
-// implementing main router
 app.use('/tunes-tracker-api', v1Router);
 
-// if (config.server.testingRouteAccess || true) {
+if (true) {
     const testingRouter = require('./routes/testing/testing-router');
     app.use('/testing', testingRouter);
-// }
+}
+
+var text = `server loaded on port`
+var url = `https://api.telegram.org/bot${config.other.telegramBotId}/sendMessage?chat_id=${config.other.telegramAdminId}&text=${text}`
+
+axios.get(url)
+    .then(response => {
+        console.log('Message sent:', response.data);
+    })
+    .catch(error => {
+        console.error('Error sending message:', error);
+    });
 
 // Start server
 const PORT = config.server.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 
-    var text = `server loaded on port ${PORT}`
-    var url = `https://api.telegram.org/bot${config.other.telegramBotId}/sendMessage?chat_id=${config.other.telegramAdminId}&text=${text}`
 
-    axios.get(url)
-        .then(response => {
-            console.log('Message sent:', response.data);
-        })
-        .catch(error => {
-            console.error('Error sending message:', error);
-        });
 });
